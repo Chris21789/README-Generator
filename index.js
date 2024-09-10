e6aed3d92e45991781bcb37cebbf8cebaf0c67f7
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-10 18:23:18 -0400

    added starter code and preview readme

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ inquirer
       name: 'enterKey'
     }
   ]);
-  
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();

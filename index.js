4bab238635f76ef18d6502d6b9e881be1c4bf96d
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-10 21:27:57 -0400

    had to change the name of a variable that was preventing my function from working properly in the generate markdown. fixed some minor things in the markdown section. added write to file code in the index file.

--- index.js
+++ index.js
@@ -1,68 +1,74 @@
 // TODO: Include packages needed for this application
-const generateMarkdown = require('./utils/generateMarkdown')
-const inquirer = require('inquirer');
-const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
     type: "input",
     message: "What is your GitHub username?",
-    name: "githubName"
+    name: "githubName",
   },
   {
     type: "input",
     message: "What is your email address?",
-    name: "email"
+    name: "email",
   },
   {
     type: "input",
     message: "What is your project's name?",
-    name: "projectName"
+    name: "title",
   },
   {
     type: "input",
     message: "Please write a short description of your project",
-    name: "description"
+    name: "description",
   },
   {
     type: "list",
-    message: 'What kind of license should your project have?',
-    name: 'license',
-    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    message: "What kind of license should your project have?",
+    name: "license",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
   },
   {
     type: "input",
     message: "What command should be run to install dependencies?",
-    name: "dependencies"
+    name: "dependencies",
   },
   {
     type: "input",
     message: "What command should be run to run tests?",
-    name: "tests"
+    name: "tests",
   },
   {
     type: "input",
     message: "What does the user need to know about using the repo?",
-    name: "usage"
+    name: "usage",
   },
   {
     type: "input",
     message: "What does the user need to know about contributing to the repo?",
-    name: "contributing"
-  }
+    name: "contributing",
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) { 
+    const  markdown = generateMarkdown(data);
+
+    fs.writeFile(fileName, markdown, (err) => {
+      err ? console.log(err) : console.log('README Success')
+    })
+
+}
 
 // TODO: Create a function to initialize app
-function init() { 
-  inquirer
-  .prompt(questions)
-  .then((response) =>
-    console.log(response.tests)
-  );}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    writeToFile("README.md", data);
+  });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,66 +1,66 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  let licenseBadge = "";
 
-  const licenseBadge = '';
-
-  if (license === 'MIT') {
-    licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-  } else if (license === 'APACHE 2.0') {
-    licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-  } else if (license === 'GPL 3.0') {
-    licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
-  } else if (license === 'BSD 3') {
-    licenseBadge = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+  if (license === "MIT") {
+    licenseBadge =
+      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "APACHE 2.0") {
+    licenseBadge =
+      "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === "GPL 3.0") {
+    licenseBadge =
+      "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license === "BSD 3") {
+    licenseBadge =
+      "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
   } else {
-    (license === 'None');
-  } return licenseBadge;
-
+    license === "None";
+  }
+  return licenseBadge;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  let licenseLink = "";
 
-  const licenseLink = '';
-
-  if (license === 'MIT') {
-    licenseLink = 'https://opensource.org/license/MIT';
-  } else if (license === 'APACHE 2.0') {
-    licenseLink = 'https://opensource.org/licenses/Apache-2.0';
-  } else if (license === 'GPL 3.0') {
-    licenseLink = 'https://www.gnu.org/licenses/gpl-3.0';
-  } else if (license === 'BSD 3') {
-    licenseLink = 'https://opensource.org/license/BSD-3-Clause';
+  if (license === "MIT") {
+    licenseLink = "https://opensource.org/license/MIT";
+  } else if (license === "APACHE 2.0") {
+    licenseLink = "https://opensource.org/licenses/Apache-2.0";
+  } else if (license === "GPL 3.0") {
+    licenseLink = "https://www.gnu.org/licenses/gpl-3.0";
+  } else if (license === "BSD 3") {
+    licenseLink = "https://opensource.org/license/BSD-3-Clause";
   } else {
-    (license === 'None');
-  } return licenseLink;
-
- }
+    license === "None";
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { 
+function renderLicenseSection(license) {
+  let licenseBadge = renderLicenseBadge(license);
+  let licenseLink = renderLicenseLink(license);
 
-  const licenseBadge = renderLicenseBadge(license);
-  const licenseLink = renderLicenseLink(license);
+  if (license !== "None") {
+    return `
+## License
 
-  if (license !== 'None') {
-    return`
-    ## License
-
-    ${licenseBadge}<a href='${licenseLink}>${license}</a>
-    `;    
+${licenseBadge}[${license}](${licenseLink})
+    `;
   } else {
-    return '';
+    return "";
   }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-
-  const license = data.license;
-  const renderLicenseSection = renderLicenseSection(license);
+  let license = data.license;
+  let renderLicense = renderLicenseSection(license);
 
   return `
   
@@ -72,15 +72,15 @@ ${data.description}
 
 ## Table of Contents
 
-* ['Installation'] (#installation)
-* ['Usage'] (#usage)
-* ['Contributing'] (#contributing)
-* ['Tests'] (#tests)
-* ['Questions'] (#questions)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
 
 ## Installation
 
-${data.dependencies}
+>${data.dependencies}
 
 ## Usage
 
@@ -92,13 +92,13 @@ ${data.contributing}
 
 ## Tests
 
-${data.tests}
+>${data.tests}
 
 ## Questions
 
 If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at ${data.githubName}.
 
-${renderLicenseSection}
+${renderLicense}
 
 `;
 }

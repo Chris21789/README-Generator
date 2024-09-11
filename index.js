a630e26f67a52936d0bafd868e16a092a94db7a5
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-10 20:14:33 -0400

    added packages that are needed for the application. added the questions needed for the readme info. added the rendering functions for the licenses badges and links. created a function to render the licenses using the badges and links.

--- index.js
+++ index.js
@@ -1,22 +1,68 @@
-const inquirer = require('inquirer');
-
-inquirer
-  .prompt([
-    {
-      message: 'Press ENTER to continue...',
-      name: 'enterKey'
-    }
-  ]);
 // TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown')
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "githubName"
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email"
+  },
+  {
+    type: "input",
+    message: "What is your project's name?",
+    name: "projectName"
+  },
+  {
+    type: "input",
+    message: "Please write a short description of your project",
+    name: "description"
+  },
+  {
+    type: "list",
+    message: 'What kind of license should your project have?',
+    name: 'license',
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+  },
+  {
+    type: "input",
+    message: "What command should be run to install dependencies?",
+    name: "dependencies"
+  },
+  {
+    type: "input",
+    message: "What command should be run to run tests?",
+    name: "tests"
+  },
+  {
+    type: "input",
+    message: "What does the user need to know about using the repo?",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "What does the user need to know about contributing to the repo?",
+    name: "contributing"
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+  inquirer
+  .prompt(questions)
+  .then((response) =>
+    console.log(response.tests)
+  );}
 
 // Function call to initialize app
 init();
